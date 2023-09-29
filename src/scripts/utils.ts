fc340477679da9f6a51e770c74f376c7900a9278
Bryan Davis <bryandavis999@gmail.com>
2023-09-29 20:44:54 +0530

    Minor refactor

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -11,7 +11,7 @@ interface distanceProps {
 export const distance = ({p1: {x: x1, y: y1}, p2: {x: x2, y: y2}}: distanceProps) => {
   const dx = x2 - x1
   const dy = y2 - y1
-  return Math.sqrt( dx**2 + dy**2 );
+  return Math.sqrt((dx ** 2) + (dy ** 2));
 }
 
 export const axisReflection = (dist: number, threshold: number, velocity: number) => {
