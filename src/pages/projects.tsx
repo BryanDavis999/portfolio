c290fef9f0333bd866886b506c92c32962c1d793
Bryan Davis <bryandavis999@gmail.com>
2023-10-17 19:03:19 +0530

    Created a stub for experiments

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,20 @@
 import Layout from '@/components/Layout';
+import Image from 'next/image';
+
 
 const Projects = () => {
   return (
     <Layout>
-      <section className='flex flex-col justify-center items-center flex-auto'>
-        <div className='flex flex-col w-full max-w-6xl px-5'>
-          WIP : Projects
+      <section className='w-full flex justify-center my-10'>
+        <div className='w-full px-5 py-5 flex justify-center max-w-screen-xl glass_background rounded-lg'>
+          <div className='w-full max-w-screen-lg flex flex-col'>
+            <div className='md:h-60 py-4 flex flex-col md:flex-row'>
+              <div className='w-52 h-52 relative aspect-square'>
+                <Image fill={true} src='/self.jpg' alt='Experiment 1' className='rounded-2xl'/>
+              </div>
+              WIP : Experiment 1
+            </div>
+          </div>
         </div>
       </section>
     </Layout>
